919ba3ff65e83a87b65b68ee68055c96ca2dad8a
stampaaaron <43514478+stampaaaron@users.noreply.github.com>
2023-01-09 16:30:28 +0100

    Initial commit

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'react-vite-storybook-typescript-starter',
+  brandUrl: 'https://github.com/openscript-ch/react-vite-storybook-typescript-starter'
+});
