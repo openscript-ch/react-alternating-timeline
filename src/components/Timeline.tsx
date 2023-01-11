bf4508075198bc33b9c8750e83d5d987f7ba5d05
stampaaaron <aaron@stampa.ch>
2023-01-11 15:11:28 +0000

    feat: setup element referencing

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,16 +1,38 @@
-import { TimelineItem, TimelineItemProps } from './TimelineItem';
 import './Timeline.css';
+import { useRef } from 'react';
+import { TimelineItem, TimelineItemProps } from './TimelineItem';
 
 export type TimelineProps = {
   items: TimelineItemProps[];
 };
 
 export function Timeline({ items }: TimelineProps) {
+  const itemsRef = useRef<Map<Date, HTMLDivElement>>();
+
+  function getRefMap() {
+    if (!itemsRef.current) {
+      // Initialize the Map on first usage.
+      itemsRef.current = new Map();
+    }
+    return itemsRef.current;
+  }
+
   return (
     <div className="timeline">
       <div className="timeline__line" />
       {items.map((item) => (
-        <TimelineItem key={item.date.toISOString()} {...item} />
+        <div
+          ref={(node) => {
+            const map = getRefMap();
+            if (node) {
+              map.set(item.date, node);
+            } else {
+              map.delete(item.date);
+            }
+          }}
+        >
+          <TimelineItem key={item.date.toISOString()} {...item} />
+        </div>
       ))}
     </div>
   );
