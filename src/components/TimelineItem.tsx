2f307f6e84a1829f0c3ee37163283089194f1669
stampaaaron <aaron@stampa.ch>
2023-01-26 17:47:53 +0000

    refactor: make timeline marks actual elements

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -17,7 +17,10 @@ export const TimelineItem = forwardRef<HTMLDivElement, TimelineItemProps>(
   ({ className, title, date, children, dateFormat, dateLocale }, ref) => {
     return (
       <div ref={ref} className={['timeline-item', className].join(' ')}>
-        <div className="timeline-item__marker" />
+        <div className="timeline-marker">
+          <div className="timeline-marker__pointer" />
+          <div className="timeline-marker__mark" />
+        </div>
         <div className="timeline-card">
           <p className="timeline-card__date">{date instanceof Date ? format(date, dateFormat ?? 'P', { locale: dateLocale }) : date}</p>
           <p className="timeline-card__title">{title}</p>
