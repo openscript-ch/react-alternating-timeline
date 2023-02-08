5556642aa1f8b5e18b1c2fc328514ca1bf876f59
stampaaaron <aaron@stampa.ch>
2023-02-08 15:32:56 +0000

    ci: fix post build

--- postbuild.js
+++ postbuild.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-fs.copyFileSync('./LICENSE', './storybook-static/LICENSE');
-fs.copyFileSync('./package.json', './storybook-static/package.json');
-fs.copyFileSync('./README.md', './storybook-static/README.md');
+fs.copyFileSync('./LICENSE', './dist/LICENSE');
+fs.copyFileSync('./package.json', './dist/package.json');
+fs.copyFileSync('./README.md', './dist/README.md');
