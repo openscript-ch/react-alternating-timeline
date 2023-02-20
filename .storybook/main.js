1972e3be9e1f8c8aad4471f45ffa18379577c18a
Robin Bühler <git+r@obin.ch>
2023-02-20 13:01:31 +0000

    chore: enhance vite and storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,40 +1,22 @@
-const react = require('@vitejs/plugin-react');
 module.exports = {
   stories: ['../stories/*.mdx', '../src/**/*.stories.tsx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../docs'],
   addons: [
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-        transcludeMarkdown: true,
-      },
-    },
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials', options: {
+        docs: {
+          configureJSX: true,
+          transcludeMarkdown: true,
+        }
+      }
+    },
     '@storybook/addon-storysource',
   ],
-  async viteFinal(config, { configType }) {
-    config.plugins = config.plugins.filter((plugin) => !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react')));
-
-    config.plugins.push(
-      react({
-        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
-      }),
-    );
-    if (configType === 'PRODUCTION') {
-      return {
-        ...config,
-        base: './',
-      };
-    }
-    return config;
+  core: {
+    builder: '@storybook/builder-vite',
   },
   framework: {
     name: '@storybook/react-vite',
-    options: {},
-  },
-  docs: {
-    docsPage: 'automatic',
   },
 };
